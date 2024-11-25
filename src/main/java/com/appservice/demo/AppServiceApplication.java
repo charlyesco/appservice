ecf7ec1ebaff86679e9fe02663ddd6e0b71e983e
carlos escorial <carlosescorial@hotmail.com>
2024-11-25 20:04:53 +0100

    First commit

---
+++ src/main/java/com/appservice/demo/AppServiceApplication.java
@@ -0,0 +1,23 @@
+package com.appservice.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class AppServiceApplication {
+
+	private String name;
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppServiceApplication.class, args);
+	}
+	
+	 @GetMapping("/hello")
+	    public String hello() {
+		return String.format("Hello %s!", name);
+	    }
+
+}
