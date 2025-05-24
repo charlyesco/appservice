07723eec8ac0c7dbaeaf867bf82398f492c776e8
carlos escorial <carlosescorial@hotmail.com>
2025-05-24 12:37:17 +0200

    Delete dependency

--- src/main/java/com/appservice/main/configuration/ProjectSecurity.java
+++ src/main/java/com/appservice/main/configuration/ProjectSecurity.java
@@ -5,29 +5,27 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.Customizer;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
+//import org.springframework.security.web.SecurityFilterChain;
 
-@EnableWebSecurity
 @Configuration
 public class ProjectSecurity {
 
 	@Autowired
 	private UserDetailsService userDetailsService;
 
-	@Bean
-	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		return http.csrf(csrf -> csrf.disable()) // Deshabilitar CSRF para simplificar pruebas de CRUD
-				.authorizeHttpRequests(auth -> auth.requestMatchers("/public/**").permitAll() // Permitir acceso público a ciertas rutas
-				.requestMatchers("/admin/**").hasRole("ADMIN") // Restringir acceso a rutas de admin
-				.anyRequest().authenticated()) // Requiere autenticación para cualquier otra ruta
-				.formLogin(Customizer.withDefaults()).httpBasic(Customizer.withDefaults()).build();
-	}
+//	@Bean
+//	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//		return http.csrf(csrf -> csrf.disable()) // Deshabilitar CSRF para simplificar pruebas de CRUD
+//				.authorizeHttpRequests(auth -> auth.requestMatchers("/public/**").permitAll() // Permitir acceso público a ciertas rutas
+//				.requestMatchers("/admin/**").hasRole("ADMIN") // Restringir acceso a rutas de admin
+//				.anyRequest().authenticated()) // Requiere autenticación para cualquier otra ruta
+//				.formLogin(Customizer.withDefaults()).httpBasic(Customizer.withDefaults()).build();
+//	}
 
 	@Bean
 	PasswordEncoder passwordEncoder() {
